fdd6daeb4d48af56b628e6c403312497bd14d244
venkata trinadh <venkatatrinadh4444@gmail.com>
2025-06-25 17:56:02 +0530

    updated the sever component as client component

--- src/app/cattle-management/page.tsx
+++ src/app/cattle-management/page.tsx
@@ -1,19 +1,29 @@
-import { cookies } from "next/headers"
-import AllAnimals from "./AllAnimals"
+"use client";
+
+import AllAnimals from "./AllAnimals";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
+const CattleManagement = () => {
+  const API_URI = process.env.NEXT_PUBLIC_BACKEND_API_URI;
+  const [allAnimals, setAllAnimals] = useState([]) as any;
+
+  useEffect(() => {
+    axios
+      .get(`${API_URI}/api/dashboard/animal/all-animals`, {
+        withCredentials: true,
+      })
+      .then((res) => setAllAnimals(res.data.allCattles))
+      .catch((err) => toast.error(err.response.data?.message));
+  }, []);
 
 
-const CattleManagement=async()=> {
-    const token=(await cookies()).get('user_token')
-    const API_URI=process.env.NEXT_PUBLIC_BACKEND_API_URI
-    const response=await fetch(`${API_URI}/api/dashboard/animal/all-animals`,{
-        headers:{Cookie:`user_token=${token?.value}`},cache:'no-store'})
-    const allAnimals=await response.json()
+  return (
+    <div className="flex-1 overflow-hidden mr-4">
+        {allAnimals.length>0 &&  <AllAnimals allAnimalDetails={allAnimals} />}
+    </div>
+  );
+};
 
-    return (
-        <div className="flex-1 overflow-hidden mr-4">
-            <AllAnimals allAnimalDetails={allAnimals.allCattles}/>
-        </div>
-    )
-}
-
-export default CattleManagement
+export default CattleManagement;
